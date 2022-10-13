bc3d2445a20c1e90337add4be66b1b1eb992bc52
FloodHydrology <cnjones7@ua.edu>
2022-10-12 20:14:13 -0500

    intitial run!

--- R/USGS_flow_rating_analysis.R
+++ R/USGS_flow_rating_analysis.R
@@ -17,14 +17,20 @@ library(patchwork) #combine multiple plot objects
 library(dataRetrieval) #download USGS data
 library(parallel) #parallel processing
 
-#Define output dir
-output_dir<-"docs/"
+#Define dir of interest
+output_dir <- "docs/"
+data_dir   <- "data/"
+
+#Load data
+gages <- read_csv(paste0(data_dir, "gagesII.csv"))
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Step 2: Create function ------------------------------------------------------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#Initially developed by Sam
-get_prc_subGood <- function(gage_id, start_date, end_date){
+#Initially developed by Sam: https://github.com/samzipper/AIMS/blob/main/code/LowFlowAnalysis_GetGoodDaysFunction.R
+
+#Function
+fun <- function(gage_id, start_date = "1979-01-01", end_date = "2018-12-30"){
   # gage_id = USGS site number in "########"
   # start_date = first date in "YYYY-MM-DD" format
   # end_date = last date in "YYYY-MM-DD" format
@@ -52,9 +58,64 @@ get_prc_subGood <- function(gage_id, start_date, end_date){
   prc_days_good <- sum(daily_raw$X_00060_00003 < min_good_q)/length(daily_raw$X_00060_00003)
   
   # make data frame to output
-  df_out <- data.frame(minGoodQ_cfs = min_good_q,
-                       daysSubGood_prc = prc_days_good)
+  df_out <- data.frame(
+    gage_id,
+    minGoodQ_cfs = min_good_q,
+    daysSubGood_prc = prc_days_good)
   
   return(df_out)
   
 }
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Step 3: Apply Functin in Parallel --------------------------------------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#See Nate's tutorial: https://bamaecohydro.github.io/Methods/fun.html
+
+#Create wrapper function 
+error_fun<-function(n){
+  tryCatch(
+    expr = fun(gages$STAID[n]), 
+    error = function(e)
+      tibble(
+        gage_id = gages$STAID[n],
+        minGoodQ_cfs = NA,
+        daysSubGood_prc = NA)
+    )
+}  
+
+#Start timer
+t0<-Sys.time()
+
+#Determine number of processing cores available on your machine
+n.cores<-detectCores()-1
+
+#Create clusters
+cl<-makeCluster(n.cores)
+
+#Send libraries to cluster
+clusterEvalQ(cl, {
+  library(dataRetrieval)
+  library(tidyverse)
+  library(lubridate)
+})
+
+#Export data to cluter environments
+clusterExport(cl, c("fun", "gages"))
+
+#Now run function
+output<-parLapply(
+  cl=cl,
+  seq(1, nrow(gages)),
+  error_fun)
+
+#Now, bind rows from list output
+output<-output %>% bind_rows()
+
+#Stop the clusters
+stopCluster(cl)
+
+#End Time
+tf<-Sys.time()
+tf-t0
+
