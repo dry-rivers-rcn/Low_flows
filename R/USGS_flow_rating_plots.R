1af79031145fa22a60a34380d703033ce4859dc1
FloodHydrology <cnjones7@ua.edu>
2022-10-15 15:07:49 -0500

    updated plot...check out that legend!

--- R/USGS_flow_rating_plots.R
+++ R/USGS_flow_rating_plots.R
@@ -38,21 +38,61 @@ states <- states %>%
 
 #Edit gages shape
 gages <- gages %>% 
-  st_as_sf(., 
-           coords = c("LNG_GAGE", 'LAT_GAGE'), 
-           crs = '+proj=longlat +datum=WGS84 +no_defs') %>% 
-  st_transform(., crs="+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80") %>%  
   left_join(., df %>% rename(STAID = gage_id)) %>% 
-  drop_na(minGoodQ_cfs)
+  drop_na(minGoodQ_cfs) %>% 
+  st_as_sf(., 
+         coords = c("LNG_GAGE", 'LAT_GAGE'), 
+         crs = '+proj=longlat +datum=WGS84 +no_defs') %>% 
+  st_transform(., crs="+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80") 
+
+
+#Limit gages to just areas within CONUS
+gages <- gages[states,]
+
+#Add xy coordinates
+gages<-gages %>% 
+  mutate(
+    lat = st_coordinates(gages)[,1], 
+    lon = st_coordinates(gages)[,2])
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Step 3: Gage location figure -------------------------------------------------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-ggplot()+
-  geom_sf(data = states) + 
-  geom_sf(data = gages, 
-          color = '#E57200', 
-          alpha = 0.70, 
-          pch=19, 
-          cex=4) +
-  theme_bw()
+#Crate map
+gages %>%
+  mutate(
+    daysSubGood_prop = daysSubGood_prc,
+    daysSubGood_prc  = daysSubGood_prc*100) %>% 
+  arrange(daysSubGood_prc) %>% 
+  ggplot()+
+    geom_sf(data = states, lwd=0.5) + 
+    geom_point(
+      aes(
+        x=lat,
+        y=lon,
+        color = daysSubGood_prc, 
+        size =  daysSubGood_prc, 
+        alpha = daysSubGood_prc),
+      pch=16,
+      ) +
+    scale_color_distiller(
+      palette = 'Spectral',
+      direction = -1, 
+      breaks = seq(0,100,25), 
+      guide="colorbar") +
+    scale_size_continuous(
+      range = c(0.5, 1.5), 
+      breaks = seq(0,100,25)) +
+    scale_alpha_continuous(
+      range = c(0.5,.8) , 
+      breaks = seq(0,100,25)) +
+    theme_bw() +
+    theme(legend.position = "right") +
+      xlab(NULL) +
+      ylab(NULL) +
+    guides(
+      color = guide_colorbar(title="% Flow Record"),
+      size="none", 
+      alpha = "none")
+  
+      
