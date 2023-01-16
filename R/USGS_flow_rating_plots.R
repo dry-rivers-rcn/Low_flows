f2c3831c96cd6c8ab20c64baab017d0238b9eb18
FloodHydrology <cnjones7@ua.edu>
2023-01-16 11:56:12 -0600

    Updats from last session (in Oct?)

--- R/USGS_flow_rating_plots.R
+++ R/USGS_flow_rating_plots.R
@@ -39,17 +39,17 @@ states <- states %>%
   st_transform(., crs="+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80") 
 
 #Tidy price classification data
-p_class <- p_class %>% 
-  #rename gage id for join below
-  rename(STAID = gage) %>% 
-  #Remove duplicates 
-  distinct() %>% 
-  #deal with 0 infront of gage no
-  mutate(
-    n.char = nchar(STAID),
-    STAID = if_else(n.char==7, paste0("0", STAID), STAID), 
-    STAID = if_else(n.char==9, paste0("0", STAID), STAID)) %>% 
-  select(-n.char)
+# p_class <- p_class %>% 
+#   #rename gage id for join below
+#   rename(STAID = gage) %>% 
+#   #Remove duplicates 
+#   distinct() %>% 
+#   #deal with 0 infront of gage no
+#   mutate(
+#     n.char = nchar(STAID),
+#     STAID = if_else(n.char==7, paste0("0", STAID), STAID), 
+#     STAID = if_else(n.char==9, paste0("0", STAID), STAID)) %>% 
+#   select(-n.char)
   
 #Edit gages shape
 gages <- gages %>% 
@@ -125,71 +125,16 @@ map_fig <- gages %>%
       #   order = 2) +
     ) +
     theme_bw() +
-      theme(legend.position = "right") +
+      theme(
+        legend.position = 'right',
+        legend.justification = "bottom") +
       xlab(NULL) +
       ylab(NULL) 
   
 map_fig
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Step 4: Duration of flow below "good" measurement ----------------------------
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#Make plot
-daysSubGood_plot <- ggplot() + 
-  geom_density(
-    data = p_gages,
-    aes(daysSubGood_prc), 
-    fill = "#4575b4", 
-    alpha = 0.99, 
-    col = "grey30", 
-    size = 0.5) +
-  geom_density(
-    data = np_gages,
-    aes(daysSubGood_prc), 
-    fill = "#d73027", 
-    alpha = 0.90, 
-    col = "grey30", 
-    size = 0.5) +
-  theme_bw() + 
-    # scale_x_log10(
-    #   breaks = c(0.001, 0.01, 0.1, 1, 10, 100),
-    #   labels = c("0.001", "0.01","0.1", "1", "10", "100")
-    # ) +
-    coord_cartesian(xlim=c(0,25))+
-    xlab("% Flow Record")
-
-daysSubGood_plot
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Step 5: Magnitude of flow below "good" measurement ----------------------------
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#Make plot
-minGoodQ_plot <- ggplot() + 
-  geom_density(
-    data = p_gages,
-    aes(minGoodQ_cfs), 
-    fill = "#4575b4", 
-    col  = "grey30", 
-    size = 0.5,
-    alpha = 0.99) +
-  geom_density(
-    data = np_gages,
-    aes(minGoodQ_cfs), 
-    fill = "#d73027", 
-    col = "grey30",
-    alpha = 0.90, 
-    size = 0.5) +
-  theme_bw() + 
-  scale_x_log10(
-    limits = c(10^-3, 10^4),
-    breaks = trans_breaks("log10", function(x) 10^x),
-    labels = trans_format("log10", math_format(10^.x)))  +
-  xlab("Flow [cfs]")
-
-minGoodQ_plot
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Step 6: Separate gages by bins-------------------------------------------------
+# Step 4: Separate gages by bins-------------------------------------------------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 b_plot <- gages %>%
   st_drop_geometry() %>%
@@ -205,13 +150,12 @@ b_plot <- gages %>%
         vjust=-1, 
         size = 3) +
       scale_x_discrete(
-        name = "% Flow Record", 
+        name = "% Flow Record Below\nGood Flow Measurement", 
         labels = c("50-75", "75-90", "90-95", "95-100")) +
       scale_y_continuous(
-        name = "Gages",
+        name = "Number of Gages",
         expand = expansion(mult = c(0, .25))) + 
-      scale_fill_viridis_d(name = "group") +
-    theme_classic() + 
+      theme_classic() + 
     theme(
       panel.grid = element_blank(),
       panel.border = element_blank(), 
@@ -224,22 +168,106 @@ b_plot <- gages %>%
 b_plot
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Step 5: Characterize flow below "good" measurement threshold -----------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## download data
+pCodes = c("00060") # discharge = 00060, stage = 00065
+USGS_gage <- "06879650"  # USGS 06879650 KINGS C NR MANHATTAN, KS
+daily_raw <- 
+  dataRetrieval::readNWISdv(siteNumbers = USGS_gage, 
+                            parameterCd = pCodes,
+                            statCd = "00003") # daily mean
+
+# Examine 
+sw_meas <- readNWISmeas(siteNumbers = USGS_gage)
+sw_meas$discharge_cfs_cut <- cut(sw_meas$discharge_va, c(0, 0.1, 1, 5, 10, 100, 10000), include.lowest = F)
+sw_meas$measured_rating_diff <- factor(sw_meas$measured_rating_diff, levels = c("Poor", "Fair", "Good", "Excellent"))
+
+
+# subset to water years only
+daily_raw$WaterYear <- year(daily_raw$Date + days(92))
+daily_raw$WYDOY <- yday(daily_raw$Date + days(92))
+colnames(daily_raw)[colnames(daily_raw)=="X_00060_00003"] <- "discharge_cfs"
+
+daily <- 
+  daily_raw %>% 
+  subset(WaterYear >= 1980 & WaterYear <= 2021)
+
+# set "low flow is impossible" threshold
+q_thres <- 0.22 # cfs
+
+daily$toolow <- daily$discharge_cfs < q_thres
+
+# sum to annual
+annual <-
+  daily %>% 
+  group_by(WaterYear) %>% 
+  summarize(toolow_days = sum(toolow),
+            total_days = sum(is.finite(discharge_cfs)),
+            toolow_prc = toolow_days/total_days,
+            discharge_cfs_total = sum(discharge_cfs)*86400,
+            discharge_cfs_toolow = sum(discharge_cfs[toolow])*86400,
+            discharge_toolow_prc = discharge_cfs_toolow/discharge_cfs_total)
+
+# sum to annual
+tots <-
+  daily  %>% 
+  summarize(
+    toolow_days = sum(toolow),
+    total_days = sum(is.finite(discharge_cfs)),
+    toolow_prc = toolow_days/total_days,
+    discharge_cfs_total = sum(discharge_cfs)*86400,
+    discharge_cfs_toolow = sum(discharge_cfs[toolow])*86400,
+    discharge_toolow_prc = discharge_cfs_toolow/discharge_cfs_total)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Step 6: Konza Plots! -----------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Manual measurements
+p_measurements <-
+  sw_meas %>% 
+  subset(!is.na(measured_rating_diff)) %>%
+  subset(discharge_va > 0) %>% 
+  ggplot(aes(x = discharge_cfs_cut, fill = measured_rating_diff)) +
+  geom_bar() +
+  scale_x_discrete(name = "Flow [cfs]", 
+                   labels = c("< 0.1", "0.1 - 1", "1 - 5", "5 - 10", "10 - 100", "> 100")) +
+  scale_y_continuous(name = "Number of Flow\nMeasurements",
+                     expand = expansion(c(0, 0.025))) +
+  scale_fill_brewer(palette = 'Spectral') +
+  theme_classic() +
+  theme(
+    legend.position=c(0.76,0.85), 
+    legend.direction = "horizontal",
+    legend.title = element_text(size=10),
+    legend.text  = element_text(size=8),
+  ) +
+  guides(fill = guide_legend(title = NULL))
+ 
+
+p_ts_days <-
+  ggplot(annual, aes(x = WaterYear, y = toolow_prc)) +
+  geom_line(
+    color = "grey30", 
+    lty=2, 
+    lwd=1.2) +
+  geom_point(
+    pch = 21,
+    fill = "red",
+    col = "grey30", 
+    cex = 3) +
+  scale_y_continuous(
+    name = "% Annual Flow\nBelow Good\nFlow Measurement", #with\nDischarge < Lowest\nGood Measurement
+    labels = scales::percent, limits = c(0, 1)) +
+  scale_x_continuous(name = "Water Year") +
+  theme_classic()
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Step 7: Combine plots --------------------------------------------------------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-map_fig +
-  inset_element(
-    b_plot,
-    left = 0.71, 
-    bottom  = 0.50, 
-    right = unit(1, 'npc') +  unit(1, 'in'), 
-    top = unit(1, 'npc') + unit(0.4, 'in')
-  ) +
-  plot_annotation(
-    #title = "USGS 06879650, Kings Creek near Manhattan KS",
-    tag_levels = "a",
-    tag_prefix = "(",
-    tag_suffix = ")")
+#I gave up on printing plots on one output. I'm going to resort to powerpoint. 
 
+ggsave(plot = map_fig,        file = "docs/map.png",            width = 6,   height = 3.75, units="in", dpi = 300)
+ggsave(plot = b_plot,         file = "docs/b_plot.png",         width = 2.3, height = 1.75,  units="in", dpi = 300)
+ggsave(plot = p_measurements, file = "docs/p_measurements.png", width = 6,   height = 1.75, units="in", dpi = 300)
+ggsave(plot = p_ts_days,      file = "docs/p_ts_days.png",      width = 6,   height = 1.75, units="in", dpi = 300)
 
-
-ggsave("docs/USGS_flow_rating.png", width = 6, height =3.75, units ="in")
