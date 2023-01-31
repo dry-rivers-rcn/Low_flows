0fac2d604e391cc4669cf5221885190b87c975af
FloodHydrology <cnjones7@ua.edu>
2023-01-30 20:37:02 -0600

    hunting errors

--- R/1_USGS_flow_rating_analysis.R
+++ R/1_USGS_flow_rating_analysis.R
@@ -119,8 +119,161 @@ stopCluster(cl)
 tf<-Sys.time()
 tf-t0 
 
-#interrogate results
+#Export results ----------------
 df<-output %>% drop_na()
-
-#Export results
 write.csv(df, "data/SubGoodQ.csv")
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Step 4: Interrogate "fails" --------------------------------------------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# #Read results from previous analysis
+# df <- read_csv("data/SubGoodQ.csv")
+# 
+# #Identify gages where analysis failed
+# fails<-gages %>% 
+#   select(STAID) %>% 
+#   rename(gage_id = STAID) %>% 
+#   left_join(., df)  %>% 
+#   filter(is.na(minGoodQ_cfs)) %>% 
+#   select(gage_id)
+
+#Define Gages that failed
+fails<-output %>% filter(is.na(daysSubGood_prc))
+
+#How many fails
+nrow(fails)
+
+  
+#4.1 Issue #1: Could not download stage-discharge data from NWIS ---------------
+#Create functino to test for gages where NWIS does not provide stage-discharge info
+fail_1_fun<-function(n){
+
+  # identify gage_id
+  gage_id<-fails$gage_id[n]
+    
+  # get rating curve
+  sw_meas <-  dataRetrieval::readNWISmeas(siteNumbers = gage_id) 
+  
+  #Export result
+  tibble(
+    gage_id, 
+    error_1 = if_else(nrow(sw_meas)>0, 0, 1)
+  )
+}
+
+#Create wrapper function 
+error_fun<-function(n){
+  tryCatch(
+    expr = fail_1_fun(n), 
+    error = function(e)
+      tibble(
+        gage_id = fails$STAID[n],
+        error_1 = 0)
+  )
+}  
+
+#Determine number of processing cores available on your machine
+n.cores<-detectCores()-1
+
+#Create clusters
+cl<-makeCluster(n.cores)
+
+#Send libraries to cluster
+clusterEvalQ(cl, {
+  library(dataRetrieval)
+  library(tidyverse)
+  library(lubridate)
+})
+
+#Export data to cluter environments
+clusterExport(cl, c("fail_1_fun", "fails"))
+
+#Now run function
+fails_1<-parLapply(
+  cl=cl,
+  seq(1, nrow(fails)),
+  error_fun)
+
+#Now, bind rows from list output
+fails_1<- fails_1 %>% bind_rows()
+
+#Stop the clusters
+stopCluster(cl)
+
+#Determine how many errors are due to this: 
+sum(fails_1$error_1)
+  #676
+
+#remove these from the fails_tibble 
+fails<-left_join(fails, fails_1) %>% filter(error_1!=1)
+
+#4.2 Issue #2: Flow data not found within time period --------------------------
+#Create functino to test for gages where NWIS does not provide flow data
+fail_2_fun<-function(n){
+
+  # identify gage_id
+  gage_id<-fails$gage_id[n]
+  
+  # read discharge data
+  pCodes = c("00060") # discharge = 00060, stage = 00065
+  daily_raw <- 
+    dataRetrieval::readNWISdv(siteNumbers = gage_id, 
+                              parameterCd = pCodes,
+                              startDate = "1979-01-01", 
+                              endDate = "2018-12-30",
+                              statCd = "00003")
+  
+  #Export result
+  tibble(
+    gage_id, 
+    error_2 = if_else(nrow(daily_raw)>0, 0, 1)
+  )
+}
+
+#Create wrapper function 
+error_fun<-function(n){
+  tryCatch(
+    expr = fail_2_fun(n), 
+    error = function(e)
+      tibble(
+        gage_id = fails$STAID[n],
+        error_1 = 0)
+  )
+}  
+
+#Determine number of processing cores available on your machine
+n.cores<-detectCores()-1
+
+#Create clusters
+cl<-makeCluster(n.cores)
+
+#Send libraries to cluster
+clusterEvalQ(cl, {
+  library(dataRetrieval)
+  library(tidyverse)
+  library(lubridate)
+})
+
+#Export data to cluter environments
+clusterExport(cl, c("fail_2_fun", "fails"))
+
+#Now run function
+fails_2<-parLapply(
+  cl=cl,
+  seq(1, nrow(fails)),
+  error_fun)
+
+#Now, bind rows from list output
+fails_2<- fails_2 %>% bind_rows()
+
+#Stop the clusters
+stopCluster(cl)
+
+#Determine how many errors are due to this: 
+sum(fails_2$error_2)
+  #417
+
+#remove these from the fails_tibble 
+fails<-left_join(fails, fails_2) %>% filter(error_2!=1)
+
